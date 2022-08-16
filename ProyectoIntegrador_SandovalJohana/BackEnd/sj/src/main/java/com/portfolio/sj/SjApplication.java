a1c1a6d2dbf25b3f510943731d72c1f19bf7c712
Johana Sandoval <johanasandoval887@gmail.com>
2022-08-16 15:21:22 -0300

    Creacion carpeta BackEnd
    Se creo la carpeta backEnd

---
+++ ProyectoIntegrador_SandovalJohana/BackEnd/sj/src/main/java/com/portfolio/sj/SjApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.sj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SjApplication.class, args);
+	}
+
+}
