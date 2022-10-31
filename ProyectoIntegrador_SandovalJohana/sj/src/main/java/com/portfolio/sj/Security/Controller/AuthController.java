55da86e115d54361d8584084548a2768e4ab474e
Johana Sandoval <johanasandoval887@gmail.com>
2022-10-31 10:30:48 -0300

    CORS BE

--- ProyectoIntegrador_SandovalJohana/sj/src/main/java/com/portfolio/sj/Security/Controller/AuthController.java
+++ ProyectoIntegrador_SandovalJohana/sj/src/main/java/com/portfolio/sj/Security/Controller/AuthController.java
@@ -34,7 +34,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/auth")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://joha-portfolio-hosting.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
